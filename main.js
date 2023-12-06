7af8572e1c66e222c877c7217a7e9f36edd24d0e
Ryandasilva44 <127805916+Ryandasilva44@users.noreply.github.com>
2023-12-06 00:58:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+ function ToggleMenu(){
+     let menuList = 
+     document.getElementById("menu-list");
+     if(menuList.style.display === "none" || 
+     menuList.style.display === "")
+     {
+         menuList.style.display = "block";
+     } else{
+         menuList.style.display = "none";
+     }
+    }
+    
+    
+    // carousel de imagens
+  
